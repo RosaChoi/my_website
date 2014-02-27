55d2e9c1ec2e559e3b71227dd07bb74b7f2c3d95
waselrosa <rosa@waselrosa.com>
2014-02-26 22:37:24 -0800

    milestone3_site page 3, 4, 5 css, top nav toggle function and snap animation finished

--- js/main.js
+++ js/main.js
@@ -1,20 +1,45 @@
-//wire up event handler
-$('.dropdown-work').click(workClicked);
-	function workClicked() {
-	console.log('workClicked');
-}
-//
-$('.dropdown-work').click(function(){
-	if($('.slidedown').is(':hidden')) {
-		$('.slidedown').slideDown(150);
-	} else {
-		$('slidedown').hide();
-	}
+//top nav toggle
+$(document).ready(function () {
+    $(".dropdown-work").on('mouseover', function () {
+        $('.slidedown').slideToggle('300');
+    });
+    $('.dropdown-work').off('mouseover', function () {
+        $('.slidedown').slideToggle('200');
+    });
 });
 
-$('#contact').click(contactClicked);
-function contactClicked() {
-	console.log('contactClicked')
-	var targetY = $('.slogan').offset().top;
-	$('body').animate({scrollTop: targetY}, 900);
+//snap to Sixty-six Days from top nav menu//
+$('#sixtysix').click(sixtysixClicked);
+function sixtysixClicked() {
+	console.log('sixtysixClicked')
+	var targetY = $('.work-sixtysix').offset().top;
+	$('body').animate({scrollTop: targetY}, 600);
 }
+
+//snap to The Land of Unlikeness from top nav menu//
+$('#unlikeness').click(unlikenessClicked);
+function unlikenessClicked() {
+	console.log('unlikenessClicked')
+	var targetY = $('.work-unlikeness').offset().top;
+	$('body').animate({scrollTop: targetY}, 600);
+}
+
+//snap to The Time Locket from top nav menu//
+$('#locket').click(locketClicked);
+function locketClicked() {
+	console.log('locketClicked')
+	var targetY = $('.work-timelocket').offset().top;
+	$('body').animate({scrollTop: targetY}, 600);
+}
+
+
+
+
+
+
+//$('#contact').click(contactClicked);
+//function contactClicked() {
+//	console.log('contactClicked')
+//	var targetY = $('.slogan').offset().top;
+//	$('body').animate({scrollTop: targetY}, 900);
+//}
