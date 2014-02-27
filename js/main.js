e68ccd9019b6c86d413dcc58491a772fa1a299cc
waselrosa <rosa@waselrosa.com>
2014-02-27 00:15:52 -0800

    milestone_site page 6,7, snap using jQuery finished.

--- js/main.js
+++ js/main.js
@@ -10,36 +10,40 @@ $(document).ready(function () {
 
 //snap to Sixty-six Days from top nav menu//
 $('#sixtysix').click(sixtysixClicked);
-function sixtysixClicked() {
-	console.log('sixtysixClicked')
-	var targetY = $('.work-sixtysix').offset().top;
-	$('body').animate({scrollTop: targetY}, 600);
-}
+	function sixtysixClicked() {
+		console.log('sixtysixClicked')
+		var targetY = $('.work-sixtysix').offset().top;
+		$('body').animate({scrollTop: targetY}, 600);
+	}
 
 //snap to The Land of Unlikeness from top nav menu//
 $('#unlikeness').click(unlikenessClicked);
-function unlikenessClicked() {
-	console.log('unlikenessClicked')
-	var targetY = $('.work-unlikeness').offset().top;
-	$('body').animate({scrollTop: targetY}, 600);
-}
+	function unlikenessClicked() {
+		console.log('unlikenessClicked')
+		var targetY = $('.work-unlikeness').offset().top;
+		$('body').animate({scrollTop: targetY}, 600);
+	}	
 
 //snap to The Time Locket from top nav menu//
 $('#locket').click(locketClicked);
-function locketClicked() {
-	console.log('locketClicked')
-	var targetY = $('.work-timelocket').offset().top;
-	$('body').animate({scrollTop: targetY}, 600);
-}
+	function locketClicked() {
+		console.log('locketClicked')
+		var targetY = $('.work-timelocket').offset().top;
+		$('body').animate({scrollTop: targetY}, 600);
+	}
 
+//snap to My Story from top nav menu//
+$('#mystory').click(myStoryClicked);
+	function myStoryClicked() {
+		console.log('myStoryClicked')
+		var targetY = $('.my-story').offset().top;
+		$('body').animate({scrollTop: targetY}, 900);
+	}
 
-
-
-
-
-//$('#contact').click(contactClicked);
-//function contactClicked() {
-//	console.log('contactClicked')
-//	var targetY = $('.slogan').offset().top;
-//	$('body').animate({scrollTop: targetY}, 900);
-//}
+//snap to Contact from top nav menu//
+$('#contact').click(contactClicked);
+	function contactClicked() {
+		console.log('contactClicked')
+		var targetY = $('.contact-page').offset().top;
+		$('body').animate({scrollTop: targetY}, 900);
+	}
