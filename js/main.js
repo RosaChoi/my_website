99061e31434b0344cb4bcd68ef91309b7b71a1b4
waselrosa <rosa@waselrosa.com>
2014-02-28 16:02:06 -0800

    milestone7 social icons button wired and animated

--- js/main.js
+++ js/main.js
@@ -8,19 +8,11 @@ $(document).ready(function () {
     });
 });
 
-//social icon toggle
-
-
-
-
-
-
-
 var FAST_SCROLL = 600
 
 //snap to Sixty-six Days from top nav menu//
 $('#sixtysix').click(sixtysixClicked);
-function sixtysixClicked() {
+	function sixtysixClicked() {
 	console.log('sixtysixClicked')
 	var targetY = $('.work-sixtysix').offset().top;
 	$('body').animate({scrollTop: targetY}, FAST_SCROLL);
@@ -63,4 +55,41 @@ function viewButtonClicked () {
 	event.preventDefault()
 	$(this).closest('div').prev('div').slideDown()
 }
+
+//social icon toggle - Gmail
+$('#email').mouseenter(function(){	
+	$('#toggle-email').animate({top:'+=450px'}, 400);
+	$('#toggle-email').show();
+});
+$('#toggle-email').mouseleave(function(){
+	$('#toggle-email').hide();
+	$('#toggle-email').animate({top:'+=510'},200);
+});
+//social icon toggle - Twitter
+$('#twitter').mouseenter(function() {
+	$('#toggle-twitter').animate({top:'+=450px'}, 400);
+	$('#toggle-twitter').show();
+});
+$('#toggle-twitter').mouseleave(function(){
+	$('#toggle-twitter').hide();
+	$('#toggle-twitter').animate({top:'+=510'},200);
+});
+//social icon toggle - Github
+$('#github').mouseenter(function() {
+	$('#toggle-github').animate({top:'+=450px'}, 400);
+	$('#toggle-github').show();
+});
+$('#toggle-github').mouseleave(function(){
+	$('#toggle-github').hide();
+	$('#toggle-github').animate({top:'+=510'},200);
+});
+//social icon toggle - LinkedIn
+$('#linkedin').mouseenter(function() {
+	$('#toggle-linkedin').animate({top:'+=450px'}, 400);
+	$('#toggle-linkedin').show();
+});
+$('#toggle-linkedin').mouseleave(function(){
+	$('#toggle-linkedin').hide();
+	$('#toggle-linkedin').animate({top:'+=510'},200);
+});
 
